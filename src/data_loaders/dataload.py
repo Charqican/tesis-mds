62aaeff9191f94d4aff4eae362fc5216ce5c4ed6
Charqican <david_7valenzuela@hotmail.com>
2026-06-19 17:39:09 -0400

    feat(dataload) batch function now returns a tensor

--- src/data_loaders/dataload.py
+++ src/data_loaders/dataload.py
@@ -11,10 +11,9 @@ def load_labels(processed_dir: str) -> torch.Tensor:
 
 
 def load_batch_point_clouds(
-    processed_dir: str,
     k_batches: int | None = None,
     config: DataLoaderConfig = DataLoaderConfig(),
-) -> list[torch.Tensor]:
+) -> torch.Tensor:
     """
     Loads obj_batchsize_i.parquet from data/processed.
     y retorna una lista de point clouds individuales.
@@ -23,6 +22,7 @@ def load_batch_point_clouds(
         list[Tensor(N, 3)]
     """
     batch_size = config.batch_size
+    processed_dir = config.processed_dir
     if k_batches:
         files = [
             Path(processed_dir) / f"obj{batch_size}_{i}.npy" for i in range(k_batches)
@@ -31,13 +31,16 @@ def load_batch_point_clouds(
         files = sorted(Path(processed_dir).glob(f"obj{batch_size}_*.npy"))
 
     # dataload_logger.info(f"Found {len(files)} batch files in {processed_dir}")
+    point_clouds_list = []
 
-    point_clouds = []
     for f in files:
         batch = np.load(f)  # (batch_size, P, 3)
-        for point_cloud in batch:
-            point_clouds.append(torch.from_numpy(point_cloud))
+        # Convertimos todo el batch de numpy a un tensor de una vez
+        point_clouds_list.append(torch.from_numpy(batch))
+
+    # Apilamos todas las listas de tensores en un solo tensor de gran tamaño
+    # Esto resultará en un tensor de forma (Total_N, P, 3)
+    point_clouds_tensor = torch.cat(point_clouds_list, dim=0)
 
     # dataload_logger.info(f"Loaded {len(point_clouds)} point clouds")
-
-    return point_clouds
+    return point_clouds_tensor
