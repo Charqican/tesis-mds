8412f75f52f5395520403ef1040f9ccc61aa44e7
Charqican <david_7valenzuela@hotmail.com>
2026-06-19 21:59:53 -0400

    feat(path resolver) using .env variables for paths

--- src/data_loaders/config.py
+++ src/data_loaders/config.py
@@ -1,9 +1,16 @@
 from dataclasses import dataclass
+from dotenv import load_dotenv
+import os
+
+# simple path resolver
+load_dotenv()
+_DEFAULT_RAW_DIR = os.environ["DATA_RAW"]
+_DEFAULT_PROCESSED_DIR = os.environ["DATA_PROCESSED"]
 
 
 @dataclass
 class DataLoaderConfig:
-    raw_dir: str = "../data/raw"
-    processed_dir: str = "../data/processed"
+    raw_dir: str = _DEFAULT_RAW_DIR
+    processed_dir: str = _DEFAULT_PROCESSED_DIR
     mode: str = "separate"
     batch_size: int = 16
