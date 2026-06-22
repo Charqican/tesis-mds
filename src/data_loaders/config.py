eb22a26c50d075930b5b35e2c4bda78eb416cec6
Charqican <david_7valenzuela@hotmail.com>
2026-06-21 23:56:00 -0400

    feat(paths) add path to gt objs.

--- src/data_loaders/config.py
+++ src/data_loaders/config.py
@@ -6,11 +6,15 @@ import os
 load_dotenv()
 _DEFAULT_RAW_DIR = os.environ["DATA_RAW"]
 _DEFAULT_PROCESSED_DIR = os.environ["DATA_PROCESSED"]
+_DEFAULT_SYMMETRY_DIR = os.environ["DATA_SYM_PLANE"]
+_DEFAULT_SYMMETRY_PROCESSED_DIR = os.environ["DATA_SYM_PLANE_PROCESSED"]
 
 
 @dataclass
 class DataLoaderConfig:
     raw_dir: str = _DEFAULT_RAW_DIR
     processed_dir: str = _DEFAULT_PROCESSED_DIR
+    symmetry_obj_dir: str = _DEFAULT_SYMMETRY_DIR
+    symmetry_processed_dir: str = _DEFAULT_SYMMETRY_PROCESSED_DIR
     mode: str = "separate"
     batch_size: int = 16
