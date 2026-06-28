d4870f194a075f4f21eae7b59f87fd93f23ad4e8
Charqican <david_7valenzuela@hotmail.com>
2026-06-27 21:51:16 -0400

    feat(experiments) implement invariance & pointwise distances

--- src/metrics/invariance.py
+++ src/metrics/invariance.py
@@ -2,12 +2,13 @@ import torch
 from metrics.utils import get_top_L_similar_index
 
 
-def compute_symmetry_invariance(
+def _symm_invariance(
     points: torch.Tensor,  # (1000, 3) - Nube de puntos submuestreada
     features: torch.Tensor,  # (1000, D) - Features correspondientes
     normal: torch.Tensor,  # (3,) - Normal del plano
     midpoint: torch.Tensor,  # (3,) - Punto medio del plano
-) -> float:
+) -> tuple[torch.Tensor, torch.Tensor]:
+
     device = points.device
 
     # reflect points using the plane of symmetry
@@ -22,7 +23,28 @@ def compute_symmetry_invariance(
     # pair points
     features_reflected = features[idx.squeeze(1)]
 
-    # calculate point similarity
-    distance = torch.mean(torch.sum(torch.abs(features - features_reflected), dim=1))
+    return features, features_reflected
 
-    return distance.item()
+
+def compute_symmetry_invariance_pointwise(
+    points: torch.Tensor,  # (1000, 3) - Nube de puntos submuestreada
+    features: torch.Tensor,  # (1000, D) - Features correspondientes
+    normal: torch.Tensor,  # (3,) - Normal del plano
+    midpoint: torch.Tensor,  # (3,) - Punto medio del plano
+) -> torch.Tensor:
+    features, features_reflected = _symm_invariance(points, features, normal, midpoint)
+
+    return torch.sum(torch.abs(features - features_reflected), dim=1)
+
+
+def compute_symmetry_invariance(
+    points: torch.Tensor,  # (1000, 3) - Nube de puntos submuestreada
+    features: torch.Tensor,  # (1000, D) - Features correspondientes
+    normal: torch.Tensor,  # (3,) - Normal del plano
+    midpoint: torch.Tensor,  # (3,) - Punto medio del plano
+) -> float:
+    # calculate point similarity
+    distances = compute_symmetry_invariance_pointwise(
+        points, features, normal, midpoint
+    )
+    return torch.mean(distances).item()
