3cd7bea549f6eaf5f910a37323a346e3f31ded03
Charqican <david_7valenzuela@hotmail.com>
2026-07-02 20:52:39 -0400

    feat(Paths) add centralized path resolver

--- src/ProjPaths.py
+++ src/ProjPaths.py
@@ -7,9 +7,11 @@ import os
 load_dotenv()
 
 _DATA_ROOT = os.environ["DATA_ROOT"]
+_DATA_GT_PLANE_SYMM_ORI = os.environ["DATA_GT_PLANE_SYMM_ORI"]
 _DATA_GT_PLANE_SYMM = os.environ["DATA_GT_PLANE_SYMM"]
 _DATA_PC_DATASET = os.environ["DATA_PC_DATASET"]
 _DATA_FEATURES = os.environ["DATA_FEATURES"]
+_DATA_INVARIANCE = os.environ["DATA_INVARIANCE"]
 
 
 # Every file in these path expects a .npy format.
@@ -17,8 +19,10 @@ _DATA_FEATURES = os.environ["DATA_FEATURES"]
 class ProjPath:
     root: str = _DATA_ROOT
     gt_plane_symm: str = _DATA_GT_PLANE_SYMM
+    gt_plane_symm_ori: str = _DATA_GT_PLANE_SYMM_ORI
     pc_dataset: str = _DATA_PC_DATASET
     features: str = _DATA_FEATURES
+    invariance: str = _DATA_INVARIANCE
 
     def get_path_feature(self, name: str | None = None) -> Path:
         if not name:
